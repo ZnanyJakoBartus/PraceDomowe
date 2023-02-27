a89cebb3f1c5668e11b1e65ebe1dff6fa40025d4
ZnanyJakoBartus <125552854+ZnanyJakoBartus@users.noreply.github.com>
2023-02-27 20:08:20 +0100

    Add files via upload

---
+++ walidacja.js
@@ -0,0 +1,33 @@
+function walidacja(){
+    var wpis = 1;
+    var braki="";
+
+    var name = document.getElementById("imie").value;
+    var Array = name.split(" ");
+    console.log(Array.length);
+    if(Array.length == 1 ){
+        alert("podaj imię i nazwisko")
+    }
+    if(document.getElementById("imie").value == ""){
+        braki +="imie\n"
+        wpis = 0;
+    }
+    if(document.getElementById("e-mail").value == ""){
+        braki+= "e-mail\n"
+        wpis=0
+    }
+    if(document.getElementById("ocena").value == ""){
+        braki +="ocena\n";
+        wpis = 0;
+    }
+    if(document.getElementById("data").value == ""){
+        braki +="data\n";
+        wpis = 0;
+    }
+
+if(wpis==1){
+     alert("Obowiązkowe pola zostały wypełnione.")}
+    else if(wpis == 0 ) {
+    alert("Nie wypełniono obowiązkowych pól:\n" + braki)}
+
+}
